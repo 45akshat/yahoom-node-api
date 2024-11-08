82d86bac7e1f4767ef3306056c6d8d082ecef945
45akshat <akshatparmar78543@gmail.com>
2024-11-08 14:44:44 +0530

    Initial commit34111111111111111111111241

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controller/auth.controller.js");
 
+const userController = require("../controller/user.controller.js");
 
-// Routes
-router.post("/signup", authController.register);
-router.post("/signin", authController.login);
-
-
-// Apply the rate limiter only to /request-otp route
-router.post("/request-otp", authController.requestOtp);
+router.get("/profile", userController.getUserProfile);
+// router.get("/", userController.getAllUsers);
 
 module.exports = router;
