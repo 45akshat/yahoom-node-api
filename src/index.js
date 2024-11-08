d0e4affdc28cf231b125977b81dde5aa7ec2c351
45akshat <akshatparmar78543@gmail.com>
2024-11-08 13:30:29 +0530

    Initial commit34111111111111111111111

--- src/index.js
+++ src/index.js
@@ -26,8 +26,6 @@ app.get("/", (req, res) => {
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
     limit: 2, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
-    standardHeaders: true, // add the `RateLimit-*` headers to the response
-    legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
   });
   
 
