b800ba007bc437d86c35249d0bce02981767d9a5
45akshat <akshatparmar78543@gmail.com>
2024-11-08 12:23:03 +0530

    Initial commit34111111111

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,24 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const rateLimit = require("express-rate-limit");
 
 const authController = require("../controller/auth.controller.js");
 
-// Rate limiter for /auth/request-otp route
-const otpRequestLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 5, // Limit each IP to 5 requests per windowMs
-    message: {
-        message: "Too many OTP requests from this IP, please try again after 10 minutes.",
-        status: false
-    }
-});
-
-// Routes
 router.post("/signup", authController.register);
 router.post("/signin", authController.login);
-
-// Apply the rate limiter only to /request-otp route
-router.post("/request-otp", otpRequestLimiter, authController.requestOtp);
+router.post("/request-otp", authController.requestOtp);
 
 module.exports = router;
